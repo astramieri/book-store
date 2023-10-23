d6b6270622f981707d7f06e2fe95044f20f4a370
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-10-23 22:18:28 +0200

    Added MongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mondogDBURL = 'mongodb+srv://bookstore:<password>@book-store-mern.jvaqzce.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mondogDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(234).send('Welcome to Book Store MERN Stack Tutorial')
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to Book Store MERN Stack Tutorial");
 });
 
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`) // backtick: ALT + 96
+  console.log(`App is listening to port: ${PORT}`); // backtick: ALT + 96
 });
+
+mongoose
+  .connect(mondogDBURL)
+  .then(() => {})
+  .catch(() => {});
