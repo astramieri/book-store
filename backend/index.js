2c749813d84bdb90676e5553818d59d7f7720095
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-11-03 22:37:40 +0100

    Update and existing book

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.get("/", (request, response) => {
 
 app.post("/books", async (request, response) => {
   try {
-    console.log(request.body);
+    //console.log(request.body);
     if (
       !request.body.title ||
       !request.body.author ||
@@ -47,8 +47,8 @@ app.get("/books", async (request, response) => {
 
     return response.status(200).json({
       count: books.length,
-      data: books
-    }); 
+      data: books,
+    });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
@@ -58,12 +58,38 @@ app.get("/books", async (request, response) => {
 // route to get one book from database by ID
 app.get("/books/:id", async (request, response) => {
   try {
-    const {id} = request.params;
+    const { id } = request.params;
 
     const book = await Book.findById(id);
 
     return response.status(200).json(book);
-     
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({message: "Book not found"});
+    }
+
+    return response.status(200).send({message: "Book updated successfully"});
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
